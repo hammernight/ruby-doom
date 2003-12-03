511af06d78b96fdfdbc52c63f00de6f4a32c846e
tom <tom@9c6a2b45-160a-0410-8aab-db8519d1476a>
2003-12-03 20:32:59 +0000

    Initial revision
    git-svn-id: http://ruby-doom.rubyforge.org/svn/trunk/ruby-doom@26 9c6a2b45-160a-0410-8aab-db8519d1476a

---
+++ etc/upload_doc.sh
@@ -0,0 +1,1 @@
+scp -r ../www/* tom@rubyforge.org:/var/www/gforge-projects/ruby-doom/
---
+++ src/read.rb
@@ -0,0 +1,35 @@
+#!/usr/local/bin/ruby
+
+class Wad
+	def initialize(filename, verbose=false)
+		@verbose = verbose
+		@type = ""
+		@bytes = []
+
+		puts "Parsing WAD file " + filename unless !@verbose
+
+		file = File.new(filename)
+		file.each_byte {|b|
+			@bytes << b
+		}
+		
+		puts "Done parsing" unless !@verbose
+	end
+	def pwad
+		type = ""
+		0.upto(3) {|x|
+			type << @bytes[x].chr
+		}
+		type=="PWAD"
+	end
+	def lumps
+		@bytes.slice(4,4).pack("V").unpack("V")
+	end
+end
+
+if __FILE__ == $0
+	w = Wad.new("../etc/simple.wad", true)
+	puts "It's a patch WAD" unless !w.pwad
+	puts "It's got " + w.lumps.to_s + " lumps"
+end
+
