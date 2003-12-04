02b7a3a58a9af8549c9ebc4717077c41f383e6f4
tom <tom@9c6a2b45-160a-0410-8aab-db8519d1476a>
2003-12-04 20:32:18 +0000

    More abstraction, same functionality
    git-svn-id: http://ruby-doom.rubyforge.org/svn/trunk/ruby-doom@48 9c6a2b45-160a-0410-8aab-db8519d1476a

--- src/ruby/doom.rb
+++ src/ruby/doom.rb
@@ -17,8 +17,16 @@ class Lump
 	end
 end
 
+class Header
+	attr_reader :type, :directory_offset
+	def initialize(array)
+		@type = Wad.convert_string(array.slice(0,4))
+		@directory_offset = Wad.convert_long(array.slice(8,4))	
+	end
+end
+
 class Wad
-	attr_reader :bytes, :lumps, :directory_offset, :lumps
+	attr_reader :lumps, :directory_offset, :lumps, :header
 	def initialize(filename, verbose=false)
 		@verbose = verbose
 		@type = ""
@@ -33,17 +41,15 @@ class Wad
 		}
 
 		puts "Done reading, building the object model" unless !@verbose
-
-		@type = Wad.convert_string(@bytes.slice(0,4))
-		lump_count = Wad.convert_long(@bytes.slice(4,4))
-		@directory_offset = Wad.convert_long(@bytes.slice(8,4))
+	
+		@header = Header.new(@bytes.slice(0,12))
 		
-		ptr = @directory_offset
-		while ptr < byte_count-15
+		directory_entry_index = @header.directory_offset
+		while directory_entry_index < byte_count-15
 			lump = Lump.new
-			lump.read(@bytes.slice(ptr,16))
+			lump.read(@bytes.slice(directory_entry_index,16))
 			@lumps << lump
-			ptr += 16
+			directory_entry_index += 16
 		end
 	
 		puts "Object model built" unless !@verbose
@@ -73,7 +79,7 @@ if __FILE__ == $0
 	file = ARGV.include?("-f") ? ARGV[ARGV.index("-f") + 1] : "../../test_wads/simple.wad"
 	w = Wad.new(file, true)
 	puts "The file " + file + " is a " + w.byte_count.to_s + " byte patch WAD" unless !w.pwad
-	puts "It's got " + w.lumps.size.to_s + " lumps, the directory starts at byte " + w.directory_offset.to_s
+	puts "It's got " + w.lumps.size.to_s + " lumps, the directory starts at byte " + w.header.directory_offset.to_s
 	puts "Lump".ljust(10) + "Size ".ljust(10) + "Offset".ljust(20)
 	w.lumps.each {|lump|
 		puts lump.name.ljust(10) + lump.size.to_s.ljust(10) + lump.offset.to_s.ljust(20)
