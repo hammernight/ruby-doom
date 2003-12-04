9aff361fd378d2bd73576c403feba07b1d31b75b
tom <tom@9c6a2b45-160a-0410-8aab-db8519d1476a>
2003-12-04 20:42:58 +0000

    Renamed Lump to DirectoryIndex, since that's what it really is
    git-svn-id: http://ruby-doom.rubyforge.org/svn/trunk/ruby-doom@50 9c6a2b45-160a-0410-8aab-db8519d1476a

--- src/ruby/doom.rb
+++ src/ruby/doom.rb
@@ -1,6 +1,7 @@
 #!/usr/local/bin/ruby
 
-class Lump
+class DirectoryEntry
+	SIZE=16
 	attr_accessor :offset, :size, :name
 	def initialize(offset=nil, size=nil, name=nil)
 		@offset = offset
@@ -18,6 +19,7 @@ class Lump
 end
 
 class Header
+	SIZE=12
 	attr_reader :type, :directory_offset, :lump_count
 	def initialize(array)
 		@type = Wad.convert_string(array.slice(0,4))
@@ -27,23 +29,23 @@ class Header
 end
 
 class Wad
-	attr_reader :lumps, :directory_offset, :lumps, :header
+	attr_reader :directory_entries, :directory_offset, :lumps, :header
 	def initialize(filename, verbose=false)
 		@verbose = verbose
 		@type = ""
 		@bytes = []
-		@lumps = []
+		@directory_entries = []
 
 		puts "Reading WAD into memory" unless !@verbose
 		File.new(filename).each_byte {|b| @bytes << b }
 		puts "Done reading, building the object model" unless !@verbose
 	
-		@header = Header.new(@bytes.slice(0,12))
+		@header = Header.new(@bytes.slice(0,Header::SIZE))
 	
-		@header.lump_count.times {|lump_index|
-			lump = Lump.new
-			lump.read(@bytes.slice((lump_index*16)+@header.directory_offset,16))
-			@lumps << lump
+		@header.lump_count.times {|directory_entry_index|
+			de = DirectoryEntry.new
+			de.read(@bytes.slice((directory_entry_index*DirectoryEntry::SIZE)+@header.directory_offset,DirectoryEntry::SIZE))
+			@directory_entries << de
 		}
 	
 		puts "Object model built" unless !@verbose
@@ -73,9 +75,9 @@ if __FILE__ == $0
 	file = ARGV.include?("-f") ? ARGV[ARGV.index("-f") + 1] : "../../test_wads/simple.wad"
 	w = Wad.new(file, true)
 	puts "The file " + file + " is a " + w.byte_count.to_s + " byte patch WAD" unless !w.pwad
-	puts "It's got " + w.lumps.size.to_s + " lumps, the directory starts at byte " + w.header.directory_offset.to_s
+	puts "It's got " + w.directory_entries.size.to_s + " lumps, the directory starts at byte " + w.header.directory_offset.to_s
 	puts "Lump".ljust(10) + "Size ".ljust(10) + "Offset".ljust(20)
-	w.lumps.each {|lump|
+	w.directory_entries.each {|lump|
 		puts lump.name.ljust(10) + lump.size.to_s.ljust(10) + lump.offset.to_s.ljust(20)
 	}
 end
