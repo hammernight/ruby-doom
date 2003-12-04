663e1fe20a7cba4f88ac2aac24f6b909cecf82f9
tom <tom@9c6a2b45-160a-0410-8aab-db8519d1476a>
2003-12-04 20:26:19 +0000

    Refactoring some parsing
    git-svn-id: http://ruby-doom.rubyforge.org/svn/trunk/ruby-doom@47 9c6a2b45-160a-0410-8aab-db8519d1476a

--- src/ruby/doom.rb
+++ src/ruby/doom.rb
@@ -1,12 +1,16 @@
 #!/usr/local/bin/ruby
 
-
 class Lump
 	attr_accessor :offset, :size, :name
-	def initialize(offset, size, name)
+	def initialize(offset=nil, size=nil, name=nil)
 		@offset = offset
 		@size = size
 		@name = name
+	end
+	def read(array)
+			@offset = Wad.convert_long(array.slice(0,4))
+			@size = Wad.convert_long(array.slice(4,4))
+			@name = Wad.convert_string(array.slice(8,8))	
 	end
 	def to_s
 		@offset.to_s + "," + @size.to_s + "," + @name
@@ -30,15 +34,18 @@ class Wad
 
 		puts "Done reading, building the object model" unless !@verbose
 
-		@type = convert_string(@bytes.slice(0,4))
-		lump_count = convert_long(@bytes.slice(4,4))
-		@directory_offset = convert_long(@bytes.slice(8,4))
+		@type = Wad.convert_string(@bytes.slice(0,4))
+		lump_count = Wad.convert_long(@bytes.slice(4,4))
+		@directory_offset = Wad.convert_long(@bytes.slice(8,4))
 		
 		ptr = @directory_offset
 		while ptr < byte_count-15
-			@lumps << Lump.new(convert_long(@bytes.slice(ptr,4)), convert_long(@bytes.slice(ptr+4,4)), convert_string(@bytes.slice(ptr+8,8)))	
+			lump = Lump.new
+			lump.read(@bytes.slice(ptr,16))
+			@lumps << lump
 			ptr += 16
-		end	
+		end
+	
 		puts "Object model built" unless !@verbose
 	end
 	def pwad
@@ -47,12 +54,15 @@ class Wad
 	def byte_count
 		@bytes.size
 	end
-	def convert_string(array)
+	def save(filename)
+		puts "Saving file" unless !@verbose
+	end
+	def Wad.convert_string(array)
 		y=""
     array.each {|x| y << x.chr }
 		y.strip
 	end
-	def convert_long(array)
+	def Wad.convert_long(array)
 		y=""
 		array.each {|x| y << x.chr } 
 		y.unpack("V")[0]
@@ -63,11 +73,10 @@ if __FILE__ == $0
 	file = ARGV.include?("-f") ? ARGV[ARGV.index("-f") + 1] : "../../test_wads/simple.wad"
 	w = Wad.new(file, true)
 	puts "The file " + file + " is a " + w.byte_count.to_s + " byte patch WAD" unless !w.pwad
-	puts "It's got " + w.lumps.size.to_s + " lumps"
-	puts "The directory starts at byte " + w.directory_offset.to_s
-	puts "Lump".ljust(10) + "Size (bytes) ".ljust(15) + "Starts at offset".ljust(20)
+	puts "It's got " + w.lumps.size.to_s + " lumps, the directory starts at byte " + w.directory_offset.to_s
+	puts "Lump".ljust(10) + "Size ".ljust(10) + "Offset".ljust(20)
 	w.lumps.each {|lump|
-		puts lump.name.ljust(10) + lump.size.to_s.ljust(15) + lump.offset.to_s.ljust(20)
+		puts lump.name.ljust(10) + lump.size.to_s.ljust(10) + lump.offset.to_s.ljust(20)
 	}
 end
 
