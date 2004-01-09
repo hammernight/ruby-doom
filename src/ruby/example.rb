1fca84eb5e5b04f7880086d13ef636a1902eb921
tom <tom@9c6a2b45-160a-0410-8aab-db8519d1476a>
2004-01-09 23:57:23 +0000

    Consolidating examples
    git-svn-id: http://ruby-doom.rubyforge.org/svn/trunk/ruby-doom@281 9c6a2b45-160a-0410-8aab-db8519d1476a

--- src/ruby/example.rb
+++ src/ruby/example.rb
@@ -41,6 +41,18 @@ if __FILE__ == $0
 	
 		w.write("new.wad"	)
 	elsif ARGV.include?("-repeatingpath")
-	
+		puts "Creating a map using a repeated path"
+		p = Path.new(0, 1000)
+		p.add("e200/n200/e200/s200/e200/",6)
+		p.add("s400/")
+		p.add("w200/s200/w200/n200/w200/",6)
+		p.add("n400/")
+		m = SimpleLineMap.new p
+		m.set_player Point.new(50,900)
+		m.create_wad("new.wad")
+		if ARGV.include?("-nethack")
+		  puts p.nethack(40)
+		  puts "Map generated from " + p.to_s
+		end
 	end
 end
