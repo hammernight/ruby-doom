95bfd3065b122a43e8d0bf0bfd54c50a91134956
tom <tom@9c6a2b45-160a-0410-8aab-db8519d1476a>
2004-01-09 23:58:59 +0000

    Consolidating examples
    git-svn-id: http://ruby-doom.rubyforge.org/svn/trunk/ruby-doom@282 9c6a2b45-160a-0410-8aab-db8519d1476a

--- src/ruby/example.rb
+++ src/ruby/example.rb
@@ -40,6 +40,17 @@ if __FILE__ == $0
 		w.lumps << linedefs
 	
 		w.write("new.wad"	)
+	elsif ARGV.include?("-path")
+		puts "Creating a simple map using an encoded path"
+
+		m = SimpleLineMap.new(Path.new(0, 1000, "e300/n200/e300/s200/e800/s500/w800/s200/w300/n200/w300/n400"))
+		m.set_player Point.new(50,900)
+		550.step(900, 40) {|x| m.add_barrel Point.new(x,900) }
+		m.create_wad("new.wad")
+		if ARGV.include?("-nethack")
+		  puts m.nethack
+		  puts "Map generated from " + m.path.to_s
+		end
 	elsif ARGV.include?("-repeatingpath")
 		puts "Creating a map using a repeated path"
 		p = Path.new(0, 1000)
